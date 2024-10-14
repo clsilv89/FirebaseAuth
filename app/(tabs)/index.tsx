551d2177eb969c6fe97a11b1e16048dbd150b127
Caio Silva <caio.lsilva_ext@stone.com.br>
2024-10-14 09:32:58 -0300

    criando navegação na aplicação e conexão com banco de dados

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,28 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native';
-import { auth } from '@/components/Firebase'
-import { useState, useEffect} from 'react'
+import LandingPage from '@/routes/LandingPage';
+import Login from '@/routes/Login';
+import Logon from '@/routes/Logon';
+import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react';
-import { User } from '@/models/User.interface';
 
 export default function HomeScreen() {
+  const Stack = createStackNavigator()
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState<User>()
-
-  useEffect(() => {
-    if (user?.uid) {
-      console.log('Navegar para a home')
-    }
-  })
-
-  async function createUser() {
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((response) => {
-        setUser(response.user)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-
-  async function login() {
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .then((response) => {
-        setUser(response.user)
-        console.log(response.user)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-  
   return (
-    <View style={{ margin: 24 }}>
-      <TextInput 
-        placeholder='Email'
-        onChangeText={(text) => setEmail(text)}
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Login'
+        component={Login}
       />
-      <TextInput 
-        onChangeText={(text) => setPassword(text)}
-        placeholder='Senha'
-        secureTextEntry={true}
+      <Stack.Screen 
+        name='Logon'
+        component={Logon}
       />
-      <Button title='Criar'
-        onPress={() => createUser()}
+      <Stack.Screen 
+        name='Landing'
+        component={LandingPage}
       />
+    </Stack.Navigator>
+  )
 
-<TextInput 
-        placeholder='Email'
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput 
-        onChangeText={(text) => setPassword(text)}
-        placeholder='Senha'
-        secureTextEntry={true}
-      />
-      <Button title='Fazer Login'
-        onPress={() => login()}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
   
-});
+}
